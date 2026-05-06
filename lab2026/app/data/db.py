7b5e90e842371687c37fbb9713772d8f517099ae
Gabriele Milia <gabrimilia00@gmail.com>
2026-05-06 09:32:31 +0200

    add stuff

--- lab2026/app/data/db.py
+++ lab2026/app/data/db.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel, create_engine, Session
-
+from typing import Annotated
+from fastapi import Depends
 
 sqlite_file_name = "C:/Users/Gabriele/Documents/GitHub/g.milia/lab2026/app/data/database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -11,4 +12,12 @@ engine = create_engine(
 
 def init_database():
     SQLModel.metadata.create_all(engine)
-    
+
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
